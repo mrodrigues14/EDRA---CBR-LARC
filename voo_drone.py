fcc0987a61b0190fe3568f108bad3a4b3bafa099
Matheus Rodrigues <102708746+mrodrigues14@users.noreply.github.com>
2023-09-09 20:15:47 -0300

    Create voo_drone.py
    testes

---
+++ voo_drone.py
@@ -0,0 +1,112 @@
+import time
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleCommand, VehicleLocalPosition
+
+
+class OffboardControl(Node):
+    def __init__(self) -> None:
+        super().__init__('offboard_control_takeoff_and_land')
+
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+
+        self.offboard_control_mode_publisher = self.create_publisher(
+            OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
+        self.trajectory_setpoint_publisher = self.create_publisher(
+            TrajectorySetpoint, '/fmu/in/trajectory_setpoint', qos_profile)
+        self.vehicle_command_publisher = self.create_publisher(
+            VehicleCommand, '/fmu/in/vehicle_command', qos_profile)
+
+        self.vehicle_local_position_subscriber = self.create_subscription(
+            VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile)
+
+        self.target_height = -5.0
+        self.has_reached_target_height = False
+        self.vehicle_local_position = VehicleLocalPosition()
+
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+        self.has_armed = False
+        self.has_engaged_offboard_mode = False
+        self.has_published_position = False
+
+    def vehicle_local_position_callback(self, msg):
+        self.get_logger().info(f"Current coordinates: X: {msg.x}, Y: {msg.y}, Z: {msg.z}")
+    def arm(self):
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=1.0)
+        print('Arm command sent')
+
+    def engage_offboard_mode(self):
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=6.0)
+        self.get_logger().info("Switching to offboard mode")
+
+    def land(self):
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
+        self.get_logger().info("Switching to land mode")
+
+    def publish_offboard_control_heartbeat_signal(self):
+        msg = OffboardControlMode()
+        msg.position = True
+        msg.velocity = False
+        msg.acceleration = False
+        msg.attitude = False
+        msg.body_rate = False
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.offboard_control_mode_publisher.publish(msg)
+
+    def publish_position_setpoint(self, x: float, y: float, z: float):
+        msg = TrajectorySetpoint()
+        msg.position = [x, y, z]
+        msg.yaw = 1.57079
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.trajectory_setpoint_publisher.publish(msg)
+        self.get_logger().info(f"Publishing position setpoints {[x, y, z]}")
+
+    def publish_vehicle_command(self, command, param1=0.0, param2=0.0, param3=0.0, param4=0.0, param5=0.0, param6=0.0,
+                                param7=0.0):
+        msg = VehicleCommand()
+        msg.command = command
+        msg.param1 = param1
+        msg.param2 = param2
+        msg.param3 = param3
+        msg.param4 = param4
+        msg.param5 = param5
+        msg.param6 = param6
+        msg.param7 = param7
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.vehicle_command_publisher.publish(msg)
+
+    def timer_callback(self) -> None:
+        self.publish_offboard_control_heartbeat_signal()
+
+        self.arm()
+        self.engage_offboard_mode()
+        self.publish_position_setpoint(0.0, 0.0, self.target_height)
+
+        if self.publish_position_setpoint(0.0, 0.0, self.target_height):
+            time.sleep(3)
+            self.land()
+
+
+
+def main(args=None) -> None:
+    print('Starting offboard control node...')
+    rclpy.init(args=args)
+    offboard_control = OffboardControl()
+    rclpy.spin(offboard_control)
+    offboard_control.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
